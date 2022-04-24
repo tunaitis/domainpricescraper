0502b23c7ff8252c37d773a7ceef1808b41f3e3a
simtun <simonas.tunaitis@gmail.com>
2022-04-24 10:05:00 +0300

    Add the most popular domains according to NameCheap

--- domain/tld.go
+++ domain/tld.go
@@ -1,15 +1,20 @@
 package domain
 
 const (
-	Com = ".com"
-	Net = ".net"
-	Org = ".org"
-	Io  = ".io"
-	Dev = ".dev"
-	Eu  = ".eu"
-	Xyz = ".xyz"
+	Com  = ".com"
+	Net  = ".net"
+	Org  = ".org"
+	Co   = ".co"
+	Ai   = ".ai"
+	CoUk = ".co.uk"
+	Ca   = ".ca"
+	Me   = ".me"
+	Io   = ".io"
+	Dev  = ".dev"
+	Eu   = ".eu"
+	Xyz  = ".xyz"
 )
 
 func AllDomains() []string {
-	return []string{Com, Net, Org, Io, Dev, Eu, Xyz}
+	return []string{Com, Net, Org, Co, Ai, CoUk, Ca, Me, Io, Dev, Eu, Xyz}
 }
