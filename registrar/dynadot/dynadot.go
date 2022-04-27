6f387f07eb42bcef6bf6fac35ae6776fa4650a85
simtun <simonas.tunaitis@gmail.com>
2022-04-27 15:54:50 +0300

    Remove the colly as a dependency from Dynadot

--- registrar/dynadot/dynadot.go
+++ registrar/dynadot/dynadot.go
@@ -1,6 +1,8 @@
 package dynadot
 
 import (
+	"errors"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly/v2"
 	"github.com/tunaitis/domainpricescraper/util"
 	"strconv"
@@ -24,37 +26,40 @@ func (d Dynadot) Name() string {
 func (d Dynadot) Scrape(tld []string) (map[string]float64, error) {
 	result := make(map[string]float64)
 
-	d.colly.OnHTML(".tld-table", func(c *colly.HTMLElement) {
-		c.ForEach(".tld-content", func(i int, e *colly.HTMLElement) {
-			a := e.DOM.Find("a")
-			s := e.DOM.Find("span")
-
-			if a.Length() == 0 || s.Length() == 0 {
-				return
-			}
-
-			domain := a.Text()
-			price := s.Text()
-			price = strings.Replace(price, "$", "", 1)
-
-			var p float64
-			p, err := strconv.ParseFloat(price, 64)
-			if err != nil {
-				return
-			}
-
-			if !util.Contains(tld, domain) {
-				return
-			}
-
-			result[domain] = p
-		})
-	})
-
-	err := d.colly.Visit("https://www.dynadot.com/change_currency.html?chg_currency=USD&pg=%2Fdomain%2Ftlds.html")
+	doc, err := util.DownloadDocument("https://www.dynadot.com/change_currency.html?chg_currency=USD&pg=%2Fdomain%2Ftlds.html", nil, nil)
 	if err != nil {
 		return nil, err
 	}
+
+	table := doc.Find(".tld-table")
+	if table.Length() == 0 {
+		return nil, errors.New("table element not found")
+	}
+
+	table.Find(".tld-content").Each(func(i int, s *goquery.Selection) {
+		anchor := s.Find("a")
+		span := s.Find("span")
+
+		if anchor.Length() == 0 || span.Length() == 0 {
+			return
+		}
+
+		domain := anchor.Text()
+		price := span.Text()
+		price = strings.Replace(price, "$", "", 1)
+
+		var p float64
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			return
+		}
+
+		if !util.Contains(tld, domain) {
+			return
+		}
+
+		result[domain] = p
+	})
 
 	return result, nil
 }
