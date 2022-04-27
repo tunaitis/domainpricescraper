dc3746fead95c32ac627335df5179456e5a741b4
simtun <simonas.tunaitis@gmail.com>
2022-04-27 15:27:57 +0300

    Remove the colly as a dependency from the Google Domains

--- registrar/googledomains/googledomains.go
+++ registrar/googledomains/googledomains.go
@@ -1,15 +1,18 @@
 package googledomains
 
 import (
+	"errors"
 	"github.com/PuerkitoBio/goquery"
-	"github.com/gocolly/colly/v2"
 	"github.com/tunaitis/domainpricescraper/util"
 	"strconv"
 	"strings"
 )
 
 type GoogleDomains struct {
-	colly *colly.Collector
+}
+
+func New() *GoogleDomains {
+	return &GoogleDomains{}
 }
 
 func (g GoogleDomains) Name() string {
@@ -20,50 +23,46 @@ func (g GoogleDomains) Scrape(tld []string) (map[string]float64, error) {
 
 	result := make(map[string]float64)
 
-	g.colly.OnHTML(".nice-table.narrow", func(c *colly.HTMLElement) {
-		c.ForEach("tr", func(i int, e *colly.HTMLElement) {
-
-			var d string
-			var p float64
-
-			e.DOM.Find("td").Each(func(i int, s *goquery.Selection) {
-				if i == 0 {
-					d = s.Text()
-				}
-
-				if i == 1 {
-					t := s.Text()
-					t = strings.Replace(t, "$", "", 1)
-					t = strings.Replace(t, "USD", "", 1)
-					t = strings.Trim(t, " ")
-
-					n, err := strconv.ParseFloat(t, 64)
-					if err != nil {
-						return
-					}
-
-					p = n
-				}
-			})
-
-			if !util.Contains(tld, d) {
-				return
-			}
-
-			result[d] = p
-		})
-	})
-
-	err := g.colly.Visit("https://support.google.com/domains/answer/6010092")
+	doc, err := util.DownloadDocument("https://support.google.com/domains/answer/6010092", nil, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	return result, nil
-}
-
-func New() *GoogleDomains {
-	return &GoogleDomains{
-		colly: colly.NewCollector(),
+	table := doc.Find(".nice-table.narrow")
+	if table.Length() == 0 {
+		return nil, errors.New("table element not found")
 	}
+
+	table.Find("tr").Each(func(i int, s *goquery.Selection) {
+		var d string
+		var p float64
+
+		s.Find("td").Each(func(i int, s *goquery.Selection) {
+			if i == 0 {
+				d = s.Text()
+			}
+
+			if i == 1 {
+				t := s.Text()
+				t = strings.Replace(t, "$", "", 1)
+				t = strings.Replace(t, "USD", "", 1)
+				t = strings.Trim(t, " ")
+
+				n, err := strconv.ParseFloat(t, 64)
+				if err != nil {
+					return
+				}
+
+				p = n
+			}
+		})
+
+		if !util.Contains(tld, d) {
+			return
+		}
+
+		result[d] = p
+	})
+
+	return result, nil
 }
