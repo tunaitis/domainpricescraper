b054287f9ea68cf01a4a13a7c3c623e14152b9bd
simtun <simonas.tunaitis@gmail.com>
2022-04-30 16:09:34 +0300

    Fix an issue in the NameCom module when sometimes the json comes with quoted numbers

--- registrar/namecom/json.go
+++ registrar/namecom/json.go
@@ -3,14 +3,20 @@ package namecom
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
 )
 
 type price struct {
 	Tld   string  `json:"tld"`
-	Price float64 `json:"registration_price"`
+	Price float64 `json:"registration_price,string"`
 }
 
 func parsePrice(data []byte) ([]price, error) {
+	// put quotes around every number in the json
+	// because sometimes the json comes with quoted numbers and sometimes not
+	re := regexp.MustCompile(`(":\s*)([\d\.]+)(\s*[,}])`)
+	data = re.ReplaceAll(data, []byte(`$1"$2"$3`))
+
 	var prices []price
 	err := json.Unmarshal(data, &prices)
 	if err != nil {
